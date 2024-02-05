777cb525ba7e48c64debfe396752331479e7fdf9
sdiawa <sekoukaderisc@yahoo.fr>
2024-02-05 13:02:33 +0100

    run docker

--- src/test/java/com/example/javaspring/JavaSpringApplicationTests.java
+++ src/test/java/com/example/javaspring/JavaSpringApplicationTests.java
@@ -1,22 +1,13 @@
 package com.example.javaspring;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
 
 @SpringBootTest
 class JavaSpringApplicationTests {
 
- @Test
-    public void testGetMessage() {
-        Javaspring javaspring = new Javaspring();
-        String expectedMessage = "Welcome chez DSK.....!";
-
-        assertEquals(expectedMessage, javaspring.getMessage());
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
-
-
